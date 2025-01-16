994c71d1ae9141578a7409388f17aad78610ca88
davidurquhart05 <durquhartjacquet894@floridapoly.edu>
2025-01-16 16:26:43 +0000

    Implement the first program

---
+++ 02_variables_intro/student_info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+    int course_id = 2271;
+    int student_id = 8940;
+    printf("Name: David Urquhart\n");
+    printf("Course ID: %d\n", course_id);
+    printf("Student ID: %d\n", student_id);
+    printf("Student ID - Course ID is %d\n", student_id - course_id);
+    
+    return 0;
+}
